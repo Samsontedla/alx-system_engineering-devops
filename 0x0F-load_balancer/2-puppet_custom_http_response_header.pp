3458b2b0839239be7e07baa7eddda830946a2687
samson tedla <samitedla23@gmail.com>
2022-01-11 00:24:18 +0000

    http response with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,46 @@
 # Puppet for NginX Configuration
-exec { 'nginx stable repository download':
-command => 'sudo add-apt-repository ppa:nginx/stable',
-path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/sbin:/bin',
+# add stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-exec { 'update all packages':
-command => 'sudo apt-get update',
-path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/sbin:/bin',
+# update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
+# install nginx
 package { 'nginx':
-ensure => 'installed',
+  ensure     => 'installed',
 }
 
-exec { 'nginx allow http':
-command => "ufw allow 'Nginx HTTP'",
-path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/sbin:/bin',
+# allow HTTP
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-exec { 'chmod for www/':
-command => 'chmod -R 755 /var/www',
-path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/sbin:/bin',
+# change folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-file { '/va/www/html/index.html':
-content => 'Hello World!\n',
+# create index file
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
 }
-file { '/var/www/html/custom_404.html':
-content => 'Ceci n'est pas une page\n',
+
+# create index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
 }
-file { 'Nginx config':
-ensure  => file,
+
+# add redirection and error page
+file { 'Nginx default config file':
+  ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
   content =>
 "server {
@@ -41,7 +51,8 @@ ensure  => file,
         server_name _;
         add_header X-Served-By \$hostname;
         location / {
-                  # if file fails, fall back to displaying a 404.
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
                 try_files \$uri \$uri/ =404;
         }
         error_page 404 /404.html;
@@ -55,11 +66,14 @@ ensure  => file,
 }
 ",
 }
-exec { 'rstart nginx service':
-command => 'service nginx restart',
-path    => '/usr/local/sbin:/usr/bin:/usr/sbin:/bin',
+# restart nginx
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
+
+# start service nginx
 service { 'nginx':
-ensure  => 'running',
-require => Package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
